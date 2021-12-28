19e9d2984f0d9a51d4067dd44bfcd866d8451469
zickz4g <lucianorosende@gmail.com>
2021-12-28 12:56:42 -0300

    Desafio 3 JS

---
+++ JS/app.js
@@ -0,0 +1,40 @@
+let rep = parseInt(prompt("Ingresa tu numero y se repetira 10 veces en console.log"));
+
+for(let i = 1;i <= 10; i++){
+
+    console.log(rep)
+
+}
+
+let multiply = parseInt(prompt("Ingresa tu numero y se hara una tabla de su valor en console.log"));
+
+for(let i = 1;i <= 15; i++){
+
+    let multiplier = multiply * i
+    console.log(multiplier)
+
+    switch(multiplier){
+        case 2:
+            console.log("soy numero primo")
+            break;
+        case 3:
+            console.log("soy numero primo")
+            break;
+        case 5:
+            console.log("soy numero primo")
+            break;
+        case 7:
+            console.log("soy numero primo")
+            break;
+        case 11:
+            console.log("soy numero primo")
+            break;
+        case 13:
+            console.log("soy numero primo")
+            break;   
+        case 17:
+            console.log("soy numero primo")
+            break;                 
+    }
+
+}
---
+++ JS/while.js
@@ -0,0 +1,7 @@
+let rep = parseInt(prompt("Ingresa tu numero siempre y cuando no sea 10"));
+
+while(rep != 10){
+
+    alert("el usuario ingreso " + rep);
+    break
+}
